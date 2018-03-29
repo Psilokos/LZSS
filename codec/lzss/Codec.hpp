f130bda36cd1ccb38738b4b5bf0b4a7f75d91b66
Ψlokos <victorien.lecouviour.tuffet@gmail.com>
2018-03-29 22:35:24 +0200

    codec: add base Codec class

---
+++ codec/lzss/Codec.hpp
@@ -0,0 +1,44 @@
+#ifndef LZSS_INT_CODEC_HPP_
+# define LZSS_INT_CODEC_HPP_
+
+# include <fstream>
+# include <queue>
+# include <string>
+# include <utility>
+
+namespace lzss
+{
+    class Codec
+    {
+        protected:
+            Codec(void) = default;
+            Codec(Codec const &) = delete;
+            Codec(Codec &&) = delete;
+
+        public:
+            virtual ~Codec(void) = default;
+
+            Codec & operator=(Codec const &) = delete;
+            Codec & operator=(Codec &&) = delete;
+
+            bool addFile(std::string const & filename);
+            Codec & operator<<(std::string const & filename);
+
+            bool hasFile(void) const;
+
+            void run(void);
+            void operator()(void);
+
+            void codeFile(void);
+
+        protected:
+            virtual void codeFilePriv(std::ifstream & ifs, std::ofstream & ofs) = 0;
+
+        protected:
+            typedef std::queue<std::pair<std::ifstream, std::ofstream>> FileQueue;
+
+            FileQueue   m_fileQ;
+    };
+}
+
+#endif
